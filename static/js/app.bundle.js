4a5d870930806bfaf9e748f68516faca881a676d
mishunov <denys.mishunov@gmail.com>
2017-12-05 09:11:52 +0100

    Comment the code and comment out the functional parts

--- static/js/app.bundle.js
+++ static/js/app.bundle.js
@@ -1,42 +1,49 @@
 !function(n){function t(e){if(r[e])return r[e].exports;var o=r[e]={i:e,l:!1,exports:{}};return n[e].call(o.exports,o,o.exports,t),o.l=!0,o.exports}var r={};t.m=n,t.c=r,t.i=function(n){return n},t.d=function(n,r,e){t.o(n,r)||Object.defineProperty(n,r,{configurable:!1,enumerable:!0,get:e})},t.n=function(n){var r=n&&n.__esModule?function(){return n.default}:function(){return n};return t.d(r,"a",r),r},t.o=function(n,t){return Object.prototype.hasOwnProperty.call(n,t)},t.p="",t(t.s=1)}([function(n,t){},function(n,t,r){"use strict";var e=r(0);!function(n){n&&n.__esModule}(e)}]);
 
-// IntersectionObserver
+/**
+ * IntrsectionObserver
+ */
+
+// Observable elements:
 // const images = document.querySelectorAll('img');
-const images = document.querySelectorAll('[lazy-load]');
+// const images = document.querySelectorAll('[lazy-load]');
+
+// Config:
 
 // const config = {};
+// const config = {
+//   // If the image gets within 50px in the Y axis, start the download.
+//   rootMargin: '50px 0px',
+//   threshold: 0
+// };
 
-const config = {
-  // If the image gets within 50px in the Y axis, start the download.
-  rootMargin: '50px 0px',
-  threshold: 0
-};
-
+// Observer:
 // The observer for the images on the page
-let observer = new IntersectionObserver(onIntersection, config);
+// let observer = new IntersectionObserver(onIntersection, config);
+// images.forEach(image => {
+//   observer.observe(image);
+// });
 
-images.forEach(image => {
-  observer.observe(image);
-});
+// Observer's handler:
+// function onIntersection(entries) {
+//   // entries of type IntersectionObserverEntry
+//   entries.forEach(entry => {
+//     // 1. console.log(entry);
+//     // Are we in viewport?
+//     if (entry.intersectionRatio > 0) {
+//       // Stop watching and load the image
+//       observer.unobserve(entry.target);
+//       // console.log(`Image ${entry.target.src} is in the viewport!`);
+//       preloadImage(entry.target);
+//     }
+//   });
+// }
 
-function onIntersection(entries) {
-  // entries of type IntersectionObserverEntry
-  entries.forEach(entry => {
-    // 1. console.log(entry);
-    // Are we in viewport?
-    if (entry.intersectionRatio > 0) {
-      // Stop watching and load the image
-      observer.unobserve(entry.target);
-      // console.log(`Image ${entry.target.src} is in the viewport!`);
-      preloadImage(entry.target);
-    }
-  });
-}
-
-function preloadImage(img) {
-  const src = img.getAttribute('data-src');
-  if (!src) {
-    return;
-  }
-  img.src = src;
-}
+// Image preloading handler
+// function preloadImage(img) {
+//   const src = img.getAttribute('data-src');
+//   if (!src) {
+//     return;
+//   }
+//   img.src = src;
+// }
